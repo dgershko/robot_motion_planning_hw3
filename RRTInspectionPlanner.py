654cca7cea7c726661c6a765c9d42596ac654a6a
dgershko <dgershko@gmail.com>
2024-03-23 02:05:57 +0200

    added global dominance optimization

--- RRTInspectionPlanner.py
+++ RRTInspectionPlanner.py
@@ -7,8 +7,7 @@ from enum import Enum
 
 class OptimizationMode(Enum):
     NoOptimization = "NoOptimization"
-    LocalDominanceByCov = "LocalDominanceByCov"
-    LocalDominanceByCost = "LocalDominanceByCost"
+    LocalDominanceByCov = "LocalDominance"
     GlobalDominance = "GlobalDominance"
 
 class RRTInspectionPlanner(object):
@@ -47,11 +46,13 @@ class RRTInspectionPlanner(object):
                 if verbose and len(self.tree.vertices) % 100 == 0:
                     print(f"Iteration {num_iterations}, Number of vertices: {len(self.tree.vertices)}")
                 # local dominance optimization
-                if optimization_mode == OptimizationMode.LocalDominanceByCov or optimization_mode == OptimizationMode.LocalDominanceByCost:
+                if optimization_mode == OptimizationMode.LocalDominance:
                     near_states = self.tree.get_knn_states(new_state, int(np.log(len(self.tree.vertices))))
                     dominating_parents = self.local_dominated(new_state, near_states)
                     if len(dominating_parents) > 0:
                         self.update_parent(new_state, dominating_parents)
+                elif optimization_mode == OptimizationMode.GlobalDominance and len(self.tree.vertices) % 100 == 0:
+                    self.rewire_tree()
 
             if self.tree.max_coverage > max_coverage:
                 max_coverage = self.tree.max_coverage
@@ -111,7 +112,10 @@ class RRTInspectionPlanner(object):
         # filter out states for which the new path is longer
         near_states = [state for state in near_states if self.tree.cost_to_state(state) + np.linalg.norm(state - new_state) < self.tree.cost_to_state(new_state)]
         # filter out states for which the new path does not properly contain all the points inpected by the old path
-        near_states = [state for state in near_states if self.tree.get_total_inspected_points(state).union(self.tree.get_inspected_points(new_state)) > (self.tree.get_total_inspected_points(new_state))]
+        near_states = [state for state in near_states if 
+                       self.tree.get_total_inspected_points(state).union(self.tree.inspected_points_in_edge(state, new_state)) > 
+                       self.tree.get_total_inspected_points(new_state)
+        ]
         # filter out states for which the edge is illegal
         near_states = [state for state in near_states if self.planning_env.edge_validity_checker(state, new_state)]
         return near_states
@@ -121,15 +125,25 @@ class RRTInspectionPlanner(object):
         Return the best parent for new_state among the dominating states.
         Chooses the one with the largest inspected point set
         """
-        if self.optimization_mode == OptimizationMode.LocalDominanceByCost:
-            costs = np.linalg.norm(dominating_states - new_state, axis=1)
-            best_parent = dominating_states[np.argmin(costs)]
-        elif self.optimization_mode == OptimizationMode.LocalDominanceByCov:
-            max_inspected_points = 0
-            best_parent = None
-            for potential_parent in dominating_states:
-                potential_inspected_points = self.tree.get_total_inspected_points(potential_parent).union(self.tree.get_inspected_points(new_state))
-                if len(potential_inspected_points) > max_inspected_points:
-                    max_inspected_points = len(potential_inspected_points)
-                    best_parent = potential_parent
+        # if self.optimization_mode == OptimizationMode.LocalDominanceByCost:
+        costs = np.linalg.norm(dominating_states - new_state, axis=1)
+        best_parent = dominating_states[np.argmin(costs)]
+        # elif self.optimization_mode == OptimizationMode.LocalDominanceByCov:
+        #     max_inspected_points = 0
+        #     best_parent = None
+        #     for potential_parent in dominating_states:
+        #         potential_inspected_points = self.tree.get_total_inspected_points(potential_parent).union(self.tree.get_inspected_points(new_state))
+        #         if len(potential_inspected_points) > max_inspected_points:
+        #             max_inspected_points = len(potential_inspected_points)
+        #             best_parent = potential_parent
         self.tree.set_parent_for_state(new_state, best_parent)
+
+    def rewire_tree(self):
+        for state in self.tree.vertices.keys():
+            other_states = list(self.tree.vertices.keys())
+            other_states.remove(state)
+            # filter out states which are children of the current state
+            other_states = [other_state for other_state in other_states if state not in self.tree.path_to_state(other_state)[0]]
+            dominating_states = self.local_dominated(np.array(state), np.array(other_states))
+            if len(dominating_states) > 0:
+                self.update_parent(np.array(state), dominating_states)
